d1b798e438ac173d73312761617b01bab47dfe9a
Fawkes07 <Fawkes0721@gmail.com>
2012-06-28 00:40:53 -0400

    I added some stuff... not sure how well done it's gonna look to you

--- MCMEPVP/src/at/doebi/MCMEPVP.java
+++ MCMEPVP/src/at/doebi/MCMEPVP.java
@@ -1,30 +1,57 @@
-package at.doebi;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
+import java.util.HashMap;
 
 public class MCMEPVP extends JavaPlugin{
-	
+	//create a HashMap and a count for player teams
+	public static HashMap<String, Integer> PlayerTeams = new HashMap<String, Integer>();
+	public static int Blue = 0;
+	public static int Red = 0;
+
 	//onEnable, run when server starts
 	@Override
 	public void onEnable() {
 		//registering Listener
 		getServer().getPluginManager().registerEvents(new MCMEPVPListener(this), this);
 	}
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 		if(cmd.getName().equalsIgnoreCase("pvp")){
 			//What to do when a player types /pvp
 			String method = args[0];
 			if(method == "join"){
-				//Assign player to a team
+				//Assign player to a team with fewest members
+				//For lack of a better idea 1 means red team and 2 means blue
+				if(Blue > Red){
+					Red = Red + 1;
+					String Playername = sender.getName();
+					PlayerTeams.put(Playername, new Integer(1)); 
+				}
+				if(Blue < Red){
+					Blue = Blue + 1;
+					String Playername = sender.getName();
+					PlayerTeams.put(Playername, new Integer(2)); 
+				}
+				else{
+					@SuppressWarnings("unused")
+					boolean random = (Math.random() < 0.5);
+					if(random = true){
+						Red = Red + 1;
+						String Playername = sender.getName();
+						PlayerTeams.put(Playername, new Integer(1));
+					}
+					else{
+						Blue = Blue + 1;
+						String Playername = sender.getName();
+						PlayerTeams.put(Playername, new Integer(2)); 
+					}
+				}
 			}
 			if(method == "leave"){
+				//Assign Player to spectator if they leave
+				String Playername = sender.getName().toLowerCase();
+				MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
 			}
 			if(method == "class"){
 				String classname = args[1];
-				
 			}
 			return true;
 		}
--- MCMEPVP/src/at/doebi/MCMEPVPListener.java
+++ MCMEPVP/src/at/doebi/MCMEPVPListener.java
@@ -2,13 +2,57 @@ package at.doebi;
 
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MCMEPVPListener implements Listener{
-	
+
 	public MCMEPVPListener(MCMEPVP instance) {
 	}
-	
+
+	void onPlayerJoin(final PlayerLoginEvent event){
+		//If a new Player joins the server make him a spectator
+		String Playername = event.getPlayer().getName().toLowerCase();
+		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
+	}
+
+	void onPlayerLeave(final PlayerQuitEvent event){
+		//If Player leaves the server remove him from his team and make him a spectator
+		String Playername = event.getPlayer().getName().toLowerCase();
+		int team = MCMEPVP.PlayerTeams.get(Playername);
+		if(team == 1){
+			MCMEPVP.Red = MCMEPVP.Red - 1;
+		}
+		if(team == 2){
+			MCMEPVP.Blue = MCMEPVP.Blue - 1;
+		}
+		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
+	}
+
+	void onPlayerKick(final PlayerKickEvent event){
+		//If Player is kicked remove him from his team and make him a spectator
+		String Playername = event.getPlayer().getName().toLowerCase();
+		int team = MCMEPVP.PlayerTeams.get(Playername);
+		if(team == 1){
+			MCMEPVP.Red = MCMEPVP.Red - 1;
+		}
+		if(team == 2){
+			MCMEPVP.Blue = MCMEPVP.Blue - 1;
+		}
+		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
+	}
+
 	void onPlayerDeath(final PlayerDeathEvent event){
-		//What to do when Player dies
+		//If Player dies remove him from his team and make him a spectator
+		String Playername = event.getEntity().getName().toLowerCase();
+		int team = MCMEPVP.PlayerTeams.get(Playername);
+		if(team == 1){
+			MCMEPVP.Red = MCMEPVP.Red - 1;
+		}
+		if(team == 2){
+			MCMEPVP.Blue = MCMEPVP.Blue - 1;
+		}
+		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
 	}
 }
