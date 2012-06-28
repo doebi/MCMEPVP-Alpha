12f11c193751efd6cb273d95c72ac49cccbb4311
Fawkes07 <Fawkes0721@gmail.com>
2012-06-28 16:46:27 -0400

    Did some more work, beginning to add class saving and selecting... not sure if it'll function though.

--- MCMEPVP/src/at/doebi/MCMEPVPListener.java
+++ MCMEPVP/src/at/doebi/MCMEPVPListener.java
@@ -13,12 +13,16 @@ public class MCMEPVPListener implements Listener{
 
 	void onPlayerJoin(final PlayerLoginEvent event){
 		//If a new Player joins the server make him a spectator
+		//and make sure his inventory is empty
 		String Playername = event.getPlayer().getName().toLowerCase();
 		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
+		event.getPlayer().getInventory().clear();
 	}
 
 	void onPlayerLeave(final PlayerQuitEvent event){
-		//If Player leaves the server remove him from his team and make him a spectator
+		//If Player leaves the server remove him from his team
+		//and make him a spectator
+		//and make sure his inventory is empty
 		String Playername = event.getPlayer().getName().toLowerCase();
 		int team = MCMEPVP.PlayerTeams.get(Playername);
 		if(team == 1){
@@ -28,10 +32,13 @@ public class MCMEPVPListener implements Listener{
 			MCMEPVP.Blue = MCMEPVP.Blue - 1;
 		}
 		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
+		event.getPlayer().getInventory().clear();
 	}
 
 	void onPlayerKick(final PlayerKickEvent event){
-		//If Player is kicked remove him from his team and make him a spectator
+		//If Player is kicked remove him from his team
+		//and make him a spectator
+		//and make sure his inventory is empty
 		String Playername = event.getPlayer().getName().toLowerCase();
 		int team = MCMEPVP.PlayerTeams.get(Playername);
 		if(team == 1){
@@ -41,11 +48,16 @@ public class MCMEPVPListener implements Listener{
 			MCMEPVP.Blue = MCMEPVP.Blue - 1;
 		}
 		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
+		event.getPlayer().getInventory().clear();
 	}
 
 	void onPlayerDeath(final PlayerDeathEvent event){
-		//If Player dies remove him from his team and make him a spectator
+		//If Player dies remove him from his team
+		//and make him a spectator
+		//and make sure his inventory is empty
 		String Playername = event.getEntity().getName().toLowerCase();
+		//***I'm not sure if getEntity() will get the player or the
+		//player's killer... getPlayer() doesn't exist for PlayerDeathEvent
 		int team = MCMEPVP.PlayerTeams.get(Playername);
 		if(team == 1){
 			MCMEPVP.Red = MCMEPVP.Red - 1;
@@ -54,5 +66,6 @@ public class MCMEPVPListener implements Listener{
 			MCMEPVP.Blue = MCMEPVP.Blue - 1;
 		}
 		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
+		event.getEntity().getInventory().clear();
 	}
 }
