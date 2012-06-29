edbb0f91e621edd063595ef5653c2c7d5e7d8299
doebi <doebi3d@gmail.com>
2012-06-29 06:51:55 +0200

    some tweaks and fixes

--- MCMEPVP/src/at/doebi/MCMEPVP.java
+++ MCMEPVP/src/at/doebi/MCMEPVP.java
@@ -2,6 +2,8 @@ package at.doebi;
 
 import java.util.HashMap;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -12,7 +14,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MCMEPVP extends JavaPlugin{
 	//create a HashMap and a count for player teams
-	public static HashMap<String, Integer> PlayerTeams = new HashMap<String, Integer>();
+	public static HashMap<String, String> PlayerTeams = new HashMap<String, String>();
 	public static int Blue = 0;
 	public static int Red = 0;
 
@@ -25,70 +27,114 @@ public class MCMEPVP extends JavaPlugin{
 		//registering Listener
 		getServer().getPluginManager().registerEvents(new MCMEPVPListener(this), this);
 	}
-
+	
+	public static void addTeam(Player player,String Team){
+		//clear Inventory
+		player.getInventory().clear();
+		if(Team == "red"){
+			Red++;
+			PlayerTeams.put(player.getName(), "blue");
+		}else{
+			if(Team == "blue"){
+				Blue++;
+				PlayerTeams.put(player.getName(), "red"); 
+			}
+		}
+		
+	}
+	
+	public static void removeTeam(Player player){
+		//set player Spectator
+		PlayerTeams.put(player.getName(), "spectator");
+		//clear Inventory
+		player.getInventory().clear();
+		//adjust Playercounter
+		String Team = PlayerTeams.get(player.getName());
+		if(Team == "red"){
+			Red--;
+		}else{
+			if(Team == "blue"){
+				Blue--;
+			}
+		}
+		
+	}
+	
+	public static void sendToTeam(String Message, Player chatter) {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+        	String Team = PlayerTeams.get(chatter.getName());
+        	ChatColor Color = ChatColor.WHITE;
+        	if(Team == "red"){
+        		Color = ChatColor.RED;
+        	}
+        	if(Team == "blue"){
+        		Color = ChatColor.BLUE;
+        	}
+            if (PlayerTeams.get(player.getName()) == Team || PlayerTeams.get(player.getName()) == "spectator") {
+                player.sendMessage(Color +"[Team "+ Team + "] " + chatter.getName() + ": " + ChatColor.WHITE + Message);
+            }
+        }
+    }
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 		if(cmd.getName().equalsIgnoreCase("pvp")){
 			//Identify the player
-			String playername = sender.getName();
-			Player player = sender.getServer().getPlayer(playername);
+			Player player = sender.getServer().getPlayer(sender.getName());
 			//What to do when a player types /pvp
 			String method = args[0];
+			//JOIN
 			if(method == "join"){
 				//Assign player to a team with fewest members
-				//For lack of a better idea 1 means red team and 2 means blue
 				if(Blue > Red){
-					Red = Red + 1;
-					String Playername = sender.getName();
-					PlayerTeams.put(Playername, new Integer(1)); 
-				}
-				if(Blue < Red){
-					Blue = Blue + 1;
-					String Playername = sender.getName();
-					PlayerTeams.put(Playername, new Integer(2)); 
-				}
-				else{
-					@SuppressWarnings("unused")
-					boolean random = (Math.random() < 0.5);
-					if(random = true){
-						Red = Red + 1;
-						String Playername = sender.getName();
-						PlayerTeams.put(Playername, new Integer(1));
+					addTeam(player,"Red");
+				}else{
+					if(Blue < Red){
+						addTeam(player,"Blue");
 					}
 					else{
-						Blue = Blue + 1;
-						String Playername = sender.getName();
-						PlayerTeams.put(Playername, new Integer(2)); 
+						boolean random = (Math.random() < 0.5);
+						if(random == true){
+							addTeam(player,"Red");
+						}
+						else{
+							addTeam(player,"Blue"); 
+						}
+					}
+				}
+			}else{
+				//LEAVE
+				if(method == "leave"){
+					//Remove Player from old Team and assign to spectator if they leave
+					removeTeam(player);
+				}else{
+					//CLASS
+					if(method == "class"){
+						String classname = args[1];
+						//Allow mods to create classes by storing their inventory in a hashmap
+						//with the class as the key
+						if(classname == "set"){
+							String classtoset = args[2];
+							PlayerInventory createinventory = player.getInventory();
+							Classes.put(classtoset, createinventory);
+						}
+						else{
+							//set a player's inventory to that of the given class if it exists
+							Inventory classinventory = Classes.get(classname);
+							if(classinventory == null){
+								player.sendMessage("no such class");
+							}
+							else{
+								player.getInventory().clear();
+								ItemStack[] contents = classinventory.getContents();
+								player.getInventory().setContents(contents);
+							}
+						}
+					}else{
+						//METHOD NOT VALID
+						sender.sendMessage("Method "+method+" is no valid!");
 					}
 				}
 			}
-			if(method == "leave"){
-				//Assign Player to spectator if they leave
-				String Playername = sender.getName().toLowerCase();
-				MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
-			}
-			if(method == "class"){
-				String classname = args[1];
-				//Allow mods to create classes by storing their inventory in a hashmap
-				//with the class as the key
-				if(classname == "set"){
-					String classtoset = args[2];
-					PlayerInventory createinventory = player.getInventory();
-					Classes.put(classtoset, createinventory);
-				}
-				else{
-					//set a player's inventory to that of the given class if it exists
-					Inventory classinventory = Classes.get(classname);
-					if(classinventory == null){
-						player.sendMessage("no such class");
-					}
-					else{
-						player.getInventory().clear();
-						ItemStack[] contents = classinventory.getContents();
-						player.getInventory().setContents(contents);
-					}
-				}
-			}
-
 			return true;
 		}
 		return false; 
--- MCMEPVP/src/at/doebi/MCMEPVPListener.java
+++ MCMEPVP/src/at/doebi/MCMEPVPListener.java
@@ -2,45 +2,33 @@ package at.doebi;
 
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MCMEPVPListener implements Listener{
-
+	
 	public MCMEPVPListener(MCMEPVP instance) {
 	}
 
 	void onPlayerJoin(final PlayerLoginEvent event){
-		//If a new Player joins the server make him a spectator
-		//and make sure his inventory is empty
-		String Playername = event.getPlayer().getName().toLowerCase();
-		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
-		event.getPlayer().getInventory().clear();
+		MCMEPVP.addTeam(event.getPlayer(),"spectator");
 	}
 
 	void onPlayerLeave(final PlayerQuitEvent event){
-		//If Player leaves the server remove him from his team
-		//and make him a spectator
-		//and make sure his inventory is empty
-		String Playername = event.getPlayer().getName().toLowerCase();
-		int team = MCMEPVP.PlayerTeams.get(Playername);
-		if(team == 1){
-			MCMEPVP.Red = MCMEPVP.Red - 1;
-		}
-		if(team == 2){
-			MCMEPVP.Blue = MCMEPVP.Blue - 1;
-		}
-		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
-		event.getPlayer().getInventory().clear();
+		MCMEPVP.removeTeam(event.getPlayer());
 	}
 
-	void onPlayerKick(final PlayerKickEvent event){
+	/*
+	 * 
+	 * i think when kicking someone a "leave" event is goin to be casted too, so we would have it double
+	 * 
+	 * void onPlayerKick(final PlayerKickEvent event){
 		//If Player is kicked remove him from his team
 		//and make him a spectator
 		//and make sure his inventory is empty
 		String Playername = event.getPlayer().getName().toLowerCase();
-		int team = MCMEPVP.PlayerTeams.get(Playername);
+		String team = MCMEPVP.PlayerTeams.get(Playername);
 		if(team == 1){
 			MCMEPVP.Red = MCMEPVP.Red - 1;
 		}
@@ -49,23 +37,18 @@ public class MCMEPVPListener implements Listener{
 		}
 		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
 		event.getPlayer().getInventory().clear();
+	}*/
+	void onPlayerChat(final PlayerChatEvent event){
+		if(MCMEPVP.PlayerTeams.get(event.getPlayer().getName()) == "spectator"){
+			//Spectator chats
+			
+		}else{
+			event.setCancelled(true);
+			MCMEPVP.sendToTeam(event.getMessage(), event.getPlayer());
+		}
 	}
-
+	
 	void onPlayerDeath(final PlayerDeathEvent event){
-		//If Player dies remove him from his team
-		//and make him a spectator
-		//and make sure his inventory is empty
-		String Playername = event.getEntity().getName().toLowerCase();
-		//***I'm not sure if getEntity() will get the player or the
-		//player's killer... getPlayer() doesn't exist for PlayerDeathEvent
-		int team = MCMEPVP.PlayerTeams.get(Playername);
-		if(team == 1){
-			MCMEPVP.Red = MCMEPVP.Red - 1;
-		}
-		if(team == 2){
-			MCMEPVP.Blue = MCMEPVP.Blue - 1;
-		}
-		MCMEPVP.PlayerTeams.put(Playername, new Integer(0));
-		event.getEntity().getInventory().clear();
+		MCMEPVP.removeTeam(event.getEntity());
 	}
 }
